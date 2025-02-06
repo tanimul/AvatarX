506ea67484c5e15086805efb59cee48d56315eb8
tanimul <tanimulislam.fuad@gmail.com>
2025-02-06 13:44:43 +0600

    Introducing fixed color for name.

--- AvatarX/src/main/java/com/tanimul/avatarx/RandomColors.kt
+++ AvatarX/src/main/java/com/tanimul/avatarx/RandomColors.kt
@@ -12,14 +12,23 @@ class RandomColors(colorShade: ColorShade = ColorShade.MEDIUM) {
         recycle.addAll(colorShade.model.colors)
     }
 
+    // Get a random color from the list of colors
     fun getColor(): Int {
         if (colors.isEmpty()) {
             while (recycle.isNotEmpty()) colors.push(recycle.pop())
             Collections.shuffle(colors)
         }
+
         val color = colors.pop()
         recycle.push(color)
         return color
     }
 
+    // Get the color based on the first character of the name
+    fun getColorForName(name: String): Int {
+        val firstChar = name.firstOrNull()?.uppercaseChar() ?: return getColor()
+        val index = firstChar - 'A'
+
+        return if (index in 0 until recycle.size) recycle[index] else getColor()
+    }
 }
